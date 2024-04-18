41d9d74d6d91a78b11b1e5bb7f67b33bb9d625fd
Rustam <rustamos@list.ru>
2024-04-19 00:04:45 +0500

    added task 2

---
+++ Sim1_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите  программу,  которая  принимает  на  вход трёхзначное целое число и на выходе показывает 
+//сумму первой и последней цифры этого числа.
+//456 => 10
+//782 => 9
+//918 => 17
+
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstDigit = num / 100; //456 / 100 = 4.56
+int lastDigit = num % 10; // 456 % 10 = 450 + 6
+
+Console.WriteLine(firstDigit + lastDigit);

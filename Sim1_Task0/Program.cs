e27804c08e19afc283f9b18adec75e9dc5348eb4
Rustam <rustamos@list.ru>
2024-04-18 23:45:45 +0500

    added task1

--- Sim1_Task0/Program.cs
+++ Sim1_Task0/Program.cs
@@ -9,17 +9,14 @@
 //int num = Convert.ToInt32(num1str);
 
 Console.Write("Введите первое число");
-
 int num1 = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
-
 if(num1 == num2 * num2)
 {
-    Console.WriteLine("Первое число является квадратом второго")
+    Console.WriteLine("Первое число является квадратом второго");
 }
 else
 {
-    Console.WriteLine("Первое число не является квадратом второго")
+    Console.WriteLine("Первое число не является квадратом второго");
 }
